c1ee3119f63de63e12e31b2ef1f6e926340bd69d
Elad Zelingher <dark_leizar@walla.com>
2017-01-08 21:25:00 +0200

    WAMP2 messages

--- Autobahn/src/de/tavendo/autobahn/WampMessage.java
+++ Autobahn/src/de/tavendo/autobahn/WampMessage.java
@@ -25,15 +25,6 @@ package de.tavendo.autobahn;
  */
 public class WampMessage {
 
-   public static final int MESSAGE_TYPE_WELCOME = 0;
-   public static final int MESSAGE_TYPE_PREFIX = 1;
-   public static final int MESSAGE_TYPE_CALL = 2;
-   public static final int MESSAGE_TYPE_CALL_RESULT = 3;
-   public static final int MESSAGE_TYPE_CALL_ERROR = 4;
-   public static final int MESSAGE_TYPE_SUBSCRIBE = 5;
-   public static final int MESSAGE_TYPE_UNSUBSCRIBE = 6;
-   public static final int MESSAGE_TYPE_PUBLISH = 7;
-   public static final int MESSAGE_TYPE_EVENT = 8;
 
 
    /// Base message class.
@@ -41,131 +32,283 @@ public class WampMessage {
 
    }
 
-   /**
-    * RPC request message.
-    * Client-to-server message.
-    */
-   public static class Call extends Message {
-      public String mCallId;
-      public String mProcUri;
-      public Object[] mArgs;
+   public static class Hello extends Message {
+      public String mRealm;
+      public HelloDetails mDetails;
 
-      public Call(String callId, String procUri, int argCount) {
-         mCallId = callId;
-         mProcUri = procUri;
-         mArgs = new Object[argCount];
+      public Hello(String realm, HelloDetails details) {
+         mRealm = realm;
+         mDetails = details;
       }
    }
 
-   /**
-    * RPC success response message.
-    * Server-to-client message.
-    */
-   public static class CallResult extends Message {
-      public String mCallId;
-      public Object mResult;
-
-      public CallResult(String callId, Object result) {
-         mCallId = callId;
-         mResult = result;
-      }
-   }
-
-   /**
-    * RPC failure response message.
-    * Server-to-client message.
-    */
-   public static class CallError extends Message {
-      public String mCallId;
-      public String mErrorUri;
-      public String mErrorDesc;
-
-      public CallError(String callId, String errorUri, String errorDesc) {
-         mCallId = callId;
-         mErrorUri = errorUri;
-         mErrorDesc = errorDesc;
-      }
-   }
-
-   /**
-    * Define Welcome message.
-    * Server-to-client message.
-    */
    public static class Welcome extends Message {
-      public String mSessionId;
-      public int mProtocolVersion;
-      public String mServerIdent;
+      public long mSession;
+      public WelcomeDetails mDetails;
 
-      public Welcome(String sessionId, int protocolVersion, String serverIdent) {
-         mSessionId = sessionId;
-         mProtocolVersion = protocolVersion;
-         mServerIdent = serverIdent;
+      public Welcome(long session, WelcomeDetails details) {
+         mSession = session;
+         mDetails = details;
       }
    }
 
-   /**
-    * Define CURIE message.
-    * Server-to-client and client-to-server message.
-    */
-   public static class Prefix extends Message {
-      public String mPrefix;
-      public String mUri;
+   public static class Abort extends Message {
+      public AbortDetails mDetails;
+      public String mReason;
 
-      public Prefix(String prefix, String uri) {
-         mPrefix = prefix;
-         mUri = uri;
+      public Abort(AbortDetails details, String reason) {
+         mDetails = details;
+         mReason = reason;
       }
    }
 
-   /**
-    * Publish to topic URI request message.
-    * Client-to-server message.
-    */
+   public static class Challenge extends Message {
+      public String mAuthMethod;
+      public ChallengeExtra mExtra;
+
+      public Challenge(String authMethod, ChallengeExtra extra) {
+         mAuthMethod = authMethod;
+         mExtra = extra;
+      }
+   }
+
+   public static class Authenticate extends Message {
+      public String mSignature;
+      public AuthenticateExtra mExtra;
+
+      public Authenticate(String signature, AuthenticateExtra extra) {
+         mSignature = signature;
+         mExtra = extra;
+      }
+   }
+
+   public static class Goodbye extends Message {
+      public GoodbyeDetails mDetails;
+      public String mReason;
+
+      public Goodbye(GoodbyeDetails details, String reason) {
+         mDetails = details;
+         mReason = reason;
+      }
+   }
+
+   public static class Error extends Message {
+      public long mType;
+      public long mRequest;
+      public ErrorDetails mDetails;
+      public String mError;
+      public Object[] mArgumentsArray;
+      public Map<String, Object> mArgumentsKw;
+
+      public Error(long type, long request, ErrorDetails details, String error, Object[] argumentsArray, Map<String, Object> argumentsKw) {
+         mType = type;
+         mRequest = request;
+         mDetails = details;
+         mError = error;
+         mArgumentsArray = argumentsArray;
+         mArgumentsKw = argumentsKw;
+      }
+   }
+
    public static class Publish extends Message {
-      public String mTopicUri;
-      public Object mEvent;
+      public long mRequest;
+      public PublishOptions mOptions;
+      public String mTopic;
+      public Object[] mArgumentsArray;
+      public Map<String, Object> mArgumentsKw;
 
-      public Publish(String topicUri, Object event) {
-         mTopicUri = topicUri;
-         mEvent = event;
+      public Publish(long request, PublishOptions options, String topic, Object[] argumentsArray, Map<String, Object> argumentsKw) {
+         mRequest = request;
+         mOptions = options;
+         mTopic = topic;
+         mArgumentsArray = argumentsArray;
+         mArgumentsKw = argumentsKw;
       }
    }
 
-   /**
-    * Subscribe to topic URI request message.
-    * Client-to-server message.
-    */
+   public static class Published extends Message {
+      public long mRequest;
+      public long mPublication;
+
+      public Published(long request, long publication) {
+         mRequest = request;
+         mPublication = publication;
+      }
+   }
+
    public static class Subscribe extends Message {
-      public String mTopicUri;
+      public long mRequest;
+      public SubscribeOptions mOptions;
+      public String mTopic;
 
-      public Subscribe(String topicUri) {
-         mTopicUri = topicUri;
+      public Subscribe(long request, SubscribeOptions options, String topic) {
+         mRequest = request;
+         mOptions = options;
+         mTopic = topic;
       }
    }
 
-   /**
-    * Unsubscribe from topic URI request message.
-    * Client-to-server message.
-    */
+   public static class Subscribed extends Message {
+      public long mRequest;
+      public long mSubscription;
+
+      public Subscribed(long request, long subscription) {
+         mRequest = request;
+         mSubscription = subscription;
+      }
+   }
+
    public static class Unsubscribe extends Message {
-      public String mTopicUri;
+      public long mRequest;
+      public long mSubscription;
 
-      public Unsubscribe(String topicUri) {
-         mTopicUri = topicUri;
+      public Unsubscribe(long request, long subscription) {
+         mRequest = request;
+         mSubscription = subscription;
       }
    }
 
-   /**
-    * Event on topic URI message.
-    * Server-to-client message.
-    */
-   public static class Event extends Message {
-      public String mTopicUri;
-      public Object mEvent;
+   public static class Unsubscribed extends Message {
+      public long mRequest;
 
-      public Event(String topicUri, Object event) {
-         mTopicUri = topicUri;
-         mEvent = event;
+      public Unsubscribed(long request) {
+         mRequest = request;
+      }
+   }
+
+   public static class Event extends Message {
+      public long mSubscription;
+      public long mPublication;
+      public EventDetails mDetails;
+      public Object[] mArgumentsArray;
+      public Map<String, Object> mArgumentsKw;
+
+      public Event(long subscription, long publication, EventDetails details, Object[] argumentsArray, Map<String, Object> argumentsKw) {
+         mSubscription = subscription;
+         mPublication = publication;
+         mDetails = details;
+         mArgumentsArray = argumentsArray;
+         mArgumentsKw = argumentsKw;
+      }
+   }
+
+   public static class Call extends Message {
+      public long mRequest;
+      public CallOptions mOptions;
+      public String mProcedure;
+      public Object[] mArgumentsArray;
+      public Map<String, Object> mArgumentsKw;
+
+      public Call(long request, CallOptions options, String procedure, Object[] argumentsArray, Map<String, Object> argumentsKw) {
+         mRequest = request;
+         mOptions = options;
+         mProcedure = procedure;
+         mArgumentsArray = argumentsArray;
+         mArgumentsKw = argumentsKw;
+      }
+   }
+
+   public static class Cancel extends Message {
+      public long mRequest;
+      public CancelOptions mOptions;
+
+      public Cancel(long request, CancelOptions options) {
+         mRequest = request;
+         mOptions = options;
+      }
+   }
+
+   public static class Result extends Message {
+      public long mRequest;
+      public ResultDetails mDetails;
+      public Object[] mArgumentsArray;
+      public Map<String, Object> mArgumentsKw;
+
+      public Result(long request, ResultDetails details, Object[] argumentsArray, Map<String, Object> argumentsKw) {
+         mRequest = request;
+         mDetails = details;
+         mArgumentsArray = argumentsArray;
+         mArgumentsKw = argumentsKw;
+      }
+   }
+
+   public static class Register extends Message {
+      public long mRequest;
+      public RegisterOptions mOptions;
+      public String mProcedure;
+
+      public Register(long request, RegisterOptions options, String procedure) {
+         mRequest = request;
+         mOptions = options;
+         mProcedure = procedure;
+      }
+   }
+
+   public static class Registered extends Message {
+      public long mRequest;
+      public long mRegistration;
+
+      public Registered(long request, long registration) {
+         mRequest = request;
+         mRegistration = registration;
+      }
+   }
+
+   public static class Unregister extends Message {
+      public long mRequest;
+      public long mRegistration;
+
+      public Unregister(long request, long registration) {
+         mRequest = request;
+         mRegistration = registration;
+      }
+   }
+
+   public static class Unregistered extends Message {
+      public long mRequest;
+
+      public Unregistered(long request) {
+         mRequest = request;
+      }
+   }
+
+   public static class Invocation extends Message {
+      public long mRequest;
+      public long mRegistration;
+      public InvocationDetails mDetails;
+      public Object[] mArgumentsArray;
+      public Map<String, Object> mArgumentsKw;
+
+      public Invocation(long request, long registration, InvocationDetails details, Object[] argumentsArray, Map<String, Object> argumentsKw) {
+         mRequest = request;
+         mRegistration = registration;
+         mDetails = details;
+         mArgumentsArray = argumentsArray;
+         mArgumentsKw = argumentsKw;
+      }
+   }
+
+   public static class Interrupt extends Message {
+      public long mRequest;
+      public InterruptOptions mOptions;
+
+      public Interrupt(long request, InterruptOptions options) {
+         mRequest = request;
+         mOptions = options;
+      }
+   }
+
+   public static class Yield extends Message {
+      public long mRequest;
+      public YieldOptions mOptions;
+      public Object[] mArgumentsArray;
+      public Map<String, Object> mArgumentsKw;
+
+      public Yield(long request, YieldOptions options, Object[] argumentsArray, Map<String, Object> argumentsKw) {
+         mRequest = request;
+         mOptions = options;
+         mArgumentsArray = argumentsArray;
+         mArgumentsKw = argumentsKw;
       }
    }
 }
